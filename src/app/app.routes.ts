124302b42a85e7101359ffebfb5172490165931c
JorgeAVargasC <javargas1209@gmail.com>
2024-09-25 00:01:38 -0500

    init: base angular 18 project init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
 
-export const routes: Routes = [];
+import { DashboardComponent } from './dashboard/dashboard.component'
+import { AuthComponent } from './auth/auth.component'
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
+import { HomeComponent } from './dashboard/home/home.component'
+import { ProductsComponent } from './dashboard/products/products.component'
+
+export const routes: Routes = [
+	{ path: '', redirectTo: 'auth', pathMatch: 'full' },
+	{ path: 'auth', component: AuthComponent },
+	{
+		path: 'dashboard',
+		component: DashboardComponent,
+		children: [
+			{
+				path: 'home',
+				component: HomeComponent
+			},
+			{
+				path: 'products',
+				component: ProductsComponent
+			}
+		]
+	},
+	{ path: '404', component: PageNotFoundComponent },
+	{ path: '**', redirectTo: '404' }
+]
